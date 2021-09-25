438a8895202434bf480a5c5b1b5aa54df5aeb68f
Tim Roeder <timothyjroeder@gmail.com>
2021-09-24 18:28:27 -0600

    Completed program specific challenge

---
+++ variables.rb
@@ -0,0 +1,10 @@
+
+movie = 'Indiana Jones'
+is_entertaining = true
+movie = 'Spaceballs'
+num4 = 1
+num6 = 2
+product = num4 * num6
+favorite_foods = ['tacos', 'burritos', 'nachos']
+favorite_foods.delete_at(0)
+p movie.length
