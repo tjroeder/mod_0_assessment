c86aa027e0007cc8abd512321b42f68cdafac707
Tim Roeder <timothyjroeder@gmail.com>
2021-09-24 19:09:15 -0600

    Updated annotations

--- variables.rb
+++ variables.rb
@@ -1,4 +1,4 @@
-
+# Declare variables.
 movie = 'Indiana Jones'
 is_entertaining = true
 movie = 'Spaceballs'
@@ -6,5 +6,7 @@ num4 = 1
 num6 = 2
 product = num4 * num6
 favorite_foods = ['tacos', 'burritos', 'nachos']
+# Remove first element from favorite_foods array.
 favorite_foods.delete_at(0)
+# Print length of movie string.
 p movie.length
