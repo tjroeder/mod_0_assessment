36464f40e8bf5180e1293215358097bc2e749bda
Tim Roeder <timothyjroeder@gmail.com>
2021-09-24 18:54:53 -0600

    Created model_a.rb with dinosaur model

---
+++ models/model_a.rb
@@ -0,0 +1,11 @@
+# Dinosaur model hash.
+dinosaur = {
+  name: 'Tyrannosaurus rex',
+  fossil_age: 'Cretaceous',
+  num_arms: 2,
+  num_legs: 2,
+  carnivore: true,
+  lays_eggs: true,
+  nicknames: ['T-Rex', 'Tyrannosaurus'],
+  dig_site_locations: ['Colorado', 'Wyoming', 'New Mexico']
+}
