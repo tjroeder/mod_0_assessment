0d13c3c3bc49a90f303070e5d575ac6f4741556a
Tim Roeder <timothyjroeder@gmail.com>
2021-09-24 19:02:20 -0600

    Created model_b.rb with developer model

---
+++ models/model_b.rb
@@ -0,0 +1,11 @@
+# Declare developer hash model.
+developer = {
+  name: 'Tim',
+  previous_occupation: 'Engineer',
+  age: 30,
+  years_experience: 7,
+  is_hungry: true,
+  is_tired: true,
+  states_lived_in: ['Alaska', 'Colorado', 'New Mexico'],
+  computer_names: ['Mordin', 'lappy', 'Liara', 'Garrus', 'mac']
+}
